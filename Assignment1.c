05b79bc08081a12ae3e247ad25ca3276bad64b27
Vidul-guptaCode <vidulgupta421@gmail.com>
2023-10-14 20:13:50 -0700

    Add Program c

---
+++ Assignment1.c
@@ -0,0 +1,54 @@
+//1-//Write a program to print Hello in the first line and student in second line
+// #include<stdio.h>
+// int main()
+// {
+//     printf("hello\nStudent");
+// }
+
+//2- Write a program  to print "MysirG" on the Screen(Remember to print double quotes)
+// #include<stdio.h>
+// int main()
+// {
+//     printf("\"vidul Gupta\"");
+
+// }
+
+//3-Write a program to print \n on thescreen 
+//#include<stdio.h>
+// int main()
+// {
+//     printf("\\n");
+
+// }
+//4-Write a program to print \r on the screen
+// int main()
+// {
+//     printf("\\r");
+
+// }
+//5-Write a program  to print "Teacher's Day" print on the screen (Remember to print duble nad single Quotes)
+// #include<stdio.h>
+// int main()
+// {
+//     printf("\'\"teacher's day\"\'");
+
+// }
+// back slash (\) propertieschange kar deta hai
+
+
+
+//6-Write a program to print sum  two interger taken from user
+// #include<stdio.h>
+// int main()
+// {
+//      int a,b,c; 
+//      printf("given a values");
+//     scanf("%d%d",&a,&b);
+//     c=a+b;
+//     printf("c=%d",c);
+//     return 0;
+
+// }
+//7-Calculate Square of given number
+//8-Calculate area of recangle
+//9-Calculate area of circle
